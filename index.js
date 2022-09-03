321ef170f2d421c8cd65aba15d8a9ef8668a0109
yousufBD10 <yousufhassan.yh2@gmail.com>
2022-09-03 05:38:40 -0400

    news found items done

--- index.js
+++ index.js
@@ -19,37 +19,6 @@ const loadAllCategorise =async ()=>{
  
  }
 
-
-// const displayNews =async ()=>{
-//     const response = await fetch("https://openapi.programming-hero.com/api/news/category/01");
-//     const data = await response.json();
-//     console.log(data.data)
-//     return data.data;
-// } 
-
-// const showdisplayNews =async ()=>{
-//     const data = await  displayNews();
-//     const showDisplay = document.getElementById('showNews');
-//     for(const detail of data){
-//       console.log(detail);
-//       const {author,image_url,details,rating,title,total_view}=detail;
-//       const div = document.createElement('div');
-//        div.classList.add('grid','gap-8');
-//       div.innerHTML = `<div  class="card grid grid-cols-2 card-side bg-base-100 shadow-xl">
-//       <figure><img src="${image_url}" alt="Movie"></figure>
-//       <div class="card-body">
-//         <h2 class="card-title">${title}</h2>
-//         <p>${details}</p>
-//         <div class="card-actions justify-end">
-//           <button class="btn btn-primary">Watch</button>
-//         </div>
-//       </div>
-//     </div>
-//       `
-//       showDisplay.appendChild(div);
-//     }
-//    }
-//    showdisplayNews();
  setCategorise();
 
 const clickCategory=(category_id,)=>{
@@ -61,9 +30,18 @@ const clickCategory=(category_id,)=>{
    
 
     const showDisplay = document.getElementById('showNews');
+    showDisplay.textContent = '';
+    const totalNews = news.length;
+    const foundItems = document.getElementById('foundItem');
+    foundItems.innerText=totalNews;
+    
+  
+    
     for(const detail of news){
-      console.log(detail);
-      const {author,image_url,details,rating,title,total_view}=detail;
+       
+     
+
+      const {author,image_url,details,title,total_view}=detail;
      const detailSlice = details.slice(0,150);
       const div = document.createElement('div');
        div.classList.add('card', 'w-full', 'bg-base-100', 'shadow-xl','mt-5');
@@ -113,7 +91,7 @@ const clickCategory=(category_id,)=>{
           </div>
         
           <div class="bg-slat-400 grid justify-items-end">
-          <button class=""><svg width="24px" height="24px" viewBox="0 0 24 24" id="magicoon-Regular" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:#41416e;}</style></defs><title>arrow-right</title><g id="arrow-right-Regular"><path id="arrow-right-Regular-2" data-name="arrow-right-Regular" class="cls-1" d="M21.53,12.531l-7,7a.75.75,0,0,1-1.06-1.061l5.719-5.72H3a.75.75,0,0,1,0-1.5H19.189L13.47,5.531A.75.75,0,1,1,14.53,4.47l7,7A.75.75,0,0,1,21.53,12.531Z"/></g></svg></button>
+          <button id="showMorebtn" class=""><svg width="24px" height="24px" viewBox="0 0 24 24" id="magicoon-Regular" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:#41416e;}</style></defs><title>arrow-right</title><g id="arrow-right-Regular"><path id="arrow-right-Regular-2" data-name="arrow-right-Regular" class="cls-1" d="M21.53,12.531l-7,7a.75.75,0,0,1-1.06-1.061l5.719-5.72H3a.75.75,0,0,1,0-1.5H19.189L13.47,5.531A.75.75,0,1,1,14.53,4.47l7,7A.75.75,0,0,1,21.53,12.531Z"/></g></svg></button>
           </div>
         </div>
       </div>
@@ -122,6 +100,9 @@ const clickCategory=(category_id,)=>{
       showDisplay.appendChild(div);
     }
  }
+//  document.getElementById('showMorebtn').addEventListener('click',function(){
+
+//  })
   
   
 

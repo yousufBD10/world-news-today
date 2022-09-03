2de58dbc53855e9e2f9e4ce1bcfcb773126ce4c9
yousufBD10 <yousufhassan.yh2@gmail.com>
2022-09-03 03:50:48 -0400

    Show news section and unneccesary code remove

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ const clickCategory=(category_id,)=>{
       const {author,image_url,details,rating,title,total_view}=detail;
      const detailSlice = details.slice(0,150);
       const div = document.createElement('div');
-       div.classList.add('card', 'w-full', 'bg-base-100', 'shadow-xl');
+       div.classList.add('card', 'w-full', 'bg-base-100', 'shadow-xl','mt-5');
       div.innerHTML = ` 
       <figure><img src="${image_url}" alt="Shoes" /></figure>
       <div class="card-body">
@@ -77,13 +77,44 @@ const clickCategory=(category_id,)=>{
         <div class="grid grid-cols-4 gap-4">
           <div>
           <img class="w-1/4 rounded-full" src="${author.img}">
-          <h5>${author.name}</h5>
-          <small>${author.date}</small>
+         <div>
+         <h5>${author.name? author.name:'Unknown' }</h5>
+         <small class="opacity-60">${author.published_date}</small>
+         </div>
           </div>
-          <div>01</div>
-          <div>01</div>
+
+          <div class="flex">
+          <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <path fill="none" stroke="#000" stroke-width="2" d="M12,17 C9.27272727,17 6,14.2222222 6,12 C6,9.77777778 9.27272727,7 12,7 C14.7272727,7 18,9.77777778 18,12 C18,14.2222222 14.7272727,17 12,17 Z M11,12 C11,12.55225 11.44775,13 12,13 C12.55225,13 13,12.55225 13,12 C13,11.44775 12.55225,11 12,11 C11.44775,11 11,11.44775 11,12 Z"/>
+</svg> <span>${total_view? total_view:'No views'}M</span>
+          </div>
+
+          <div>
+          <div class="flex">
+          <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <polygon fill="#FFC95E" fill-rule="evenodd" points="12 16.667 5 22 8 14 2 9.5 9.5 9.5 12 2 14.5 9.5 22 9.5 16 14 19 22"/>
+        </svg>
+        <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <polygon fill="#FFC95E" fill-rule="evenodd" points="12 16.667 5 22 8 14 2 9.5 9.5 9.5 12 2 14.5 9.5 22 9.5 16 14 19 22"/>
+</svg>
+<svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <polygon fill="#FFC95E" fill-rule="evenodd" points="12 16.667 5 22 8 14 2 9.5 9.5 9.5 12 2 14.5 9.5 22 9.5 16 14 19 22"/>
+</svg>
+<svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <polygon fill="#FFC95E" fill-rule="evenodd" points="12 16.667 5 22 8 14 2 9.5 9.5 9.5 12 2 14.5 9.5 22 9.5 16 14 19 22"/>
+</svg>
+<svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+  <g fill="none" fill-rule="evenodd">
+    <polygon fill="#000" fill-opacity=".2" points="12 16.667 12 2 14.5 9.5 22 9.5 16 14 19 22"/>
+    <polygon fill="#FFC95E" points="12 16.667 5 22 8 14 2 9.5 9.5 9.5 12 2"/>
+  </g>
+</svg>
+        </div>
+          </div>
         
-          <div>09</div>
+          <div class="bg-slat-400 grid justify-items-end">
+          <button class=""><svg width="24px" height="24px" viewBox="0 0 24 24" id="magicoon-Regular" xmlns="http://www.w3.org/2000/svg"><defs><style>.cls-1{fill:#41416e;}</style></defs><title>arrow-right</title><g id="arrow-right-Regular"><path id="arrow-right-Regular-2" data-name="arrow-right-Regular" class="cls-1" d="M21.53,12.531l-7,7a.75.75,0,0,1-1.06-1.061l5.719-5.72H3a.75.75,0,0,1,0-1.5H19.189L13.47,5.531A.75.75,0,1,1,14.53,4.47l7,7A.75.75,0,0,1,21.53,12.531Z"/></g></svg></button>
+          </div>
         </div>
       </div>
    
